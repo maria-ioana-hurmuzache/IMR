531795c2531ce5dba7cdc46b9f415128095217db
maria-ioana-hurmuzache <maria.ioana.hurmuzache@gmail.com>
2025-10-08 16:59:41 +0300

    tema unu

---
+++ tema1/Tema1_Hurmuzache_Nistor/Assets/DistanceController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+using static UnityEngine.GraphicsBuffer;
+
+public class StateChanging : MonoBehaviour
+{
+    public Transform target1;
+    public Transform target2;
+    public Animator anim1;
+    public Animator anim2;
+    public float attackDistance = 0.25f;
+
+    private bool hasAttacked = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target1 == null || target2 == null) return;
+
+        float distance = Vector3.Distance(target1.position, target2.position);
+        bool isClose = distance < attackDistance;
+
+        if (isClose)
+        {
+            anim1.SetTrigger("isAttacking");
+            anim2.SetTrigger("isAttacking");
+            hasAttacked = true;
+        }
+        else if (!isClose)
+        {
+            // Reset attack state if they move apart
+            hasAttacked = false;
+        }
+    }
+}
